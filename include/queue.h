ab0338b2d582a85e880856da44fd6b767d38958b
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-11-22 18:26:58 -0500

    queue: Adding queue_peek().
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- include/queue.h
+++ include/queue.h
@@ -10,12 +10,14 @@ struct queue;
 /*
  * _push     - Adds an element to the back of the queue. Returns 0 on success.
  * _pop      - Removes an element from the front of the queue.
+ * _peek     - Returns the element at the front of the queue.
  * _is_empty - Returns non-zero if the queue is empty.
  */
 struct queue*  queue_create();
 void           queue_destroy(struct queue*);
 int            queue_push(struct queue*, void*);
 void*          queue_pop(struct queue*);
+void*          queue_peek(struct queue*);
 int            queue_is_empty(struct queue*);
 
 #endif
