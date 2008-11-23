dfab85ba707904f7e3c7ac5e3f54cd98d25dc3db
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-11-23 12:23:12 -0500

    pqueue: Getting better test coverage.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- test/pqueue_test.c
+++ test/pqueue_test.c
@@ -18,10 +18,11 @@ T_TEST(t_pqueue_basic) {
 T_TEST(t_pqueue_inorder_insert) {
 	struct pqueue* q = pqueue_create(comp);
 	int i;
+	int num = 10;
 
 	T_ASSERT(q);
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < num; i++) {
 		T_ASSERT(0 == pqueue_insert(q, (void*)i));
 	}
 
@@ -34,10 +35,35 @@ T_TEST(t_pqueue_inorder_insert) {
 		i++;
 	}
 
-	T_ASSERT(i == 10);
+	T_ASSERT(i == num);
 
 	pqueue_destroy(q);
 
+} T_END_TEST
+
+T_TEST(t_pqueue_alt_insert) {
+	struct pqueue* q = pqueue_create(comp);
+	int i;
+	int num = 10;
+
+	T_ASSERT(q);
+
+	for (i = 0; i < num; i++) {
+		T_ASSERT(0 == pqueue_insert(q, (void*)((i % 2) ? i : num - i - (num % 2 ? 1 : 2))));
+	}
+	
+	T_ASSERT(!pqueue_is_empty(q));
+
+	i = 0;
+	while (!pqueue_is_empty(q)) {
+		T_ASSERT(i == (int)pqueue_peek(q));
+		T_ASSERT(i == (int)pqueue_pop(q));
+		i++;
+	}
+
+	T_ASSERT(i == num);
+
+	pqueue_destroy(q);
 } T_END_TEST
 
 T_TEST(t_pqueue_duplicate) {
@@ -63,10 +89,22 @@ T_TEST(t_pqueue_duplicate) {
 
 } T_END_TEST
 
+T_TEST(t_pqueue_destroy_null) {
+	pqueue_destroy(NULL);
+} T_END_TEST
+
+T_TEST(t_pqueue_destroy_empty) {
+	struct pqueue* q = pqueue_create(comp);
+	pqueue_destroy(q);
+} T_END_TEST
+
 T_SUITE(pqueue_tests) {
 
 	T_ADD_TEST(t_pqueue_basic);
 	T_ADD_TEST(t_pqueue_inorder_insert);
+	T_ADD_TEST(t_pqueue_alt_insert);
 	T_ADD_TEST(t_pqueue_duplicate);
+	T_ADD_TEST(t_pqueue_destroy_null);
+	T_ADD_TEST(t_pqueue_destroy_empty);
 
 } T_END_SUITE
